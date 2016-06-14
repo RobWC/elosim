2683343eb30e71783055ce8a0edbaa08eedc8f3c
Rob Cameron <rwcameron@gmail.com>
2016-06-14 00:42:49 -0700

    updated formula, resonably well now

--- elo.go
+++ elo.go
@@ -2,7 +2,13 @@ package main
 
 import "math"
 
-const KFactor = 32
+const KFactorBase = 32
+const MaxElo = 2400
+const MinElo = 600
+
+func calcEloWinChance(player, opponent int) float64 {
+	return 1 / (float64(1) + math.Pow(10, float64((opponent-player)/400)))
+}
 
 func calcELO(player, opponent int, win bool) int {
 	w := 0
@@ -11,9 +17,24 @@ func calcELO(player, opponent int, win bool) int {
 		w = 1
 	}
 
-	f := 1 / (float64(1) + math.Pow(10, float64(opponent-player)/400))
+	f := 1 / (float64(1) + math.Pow(10, float64((opponent-player)/400)))
 
-	return player + int(KFactor*(float64(w)-(f)))
+	KFactor := KFactorBase
+	if player >= 1200 && player <= 2000 {
+		KFactor = KFactorBase - 8
+	} else if player > 2000 {
+		KFactor = KFactorBase - 16
+	}
+
+	e := player + int(float64(KFactor)*(float64(w)-(f)))
+
+	if e > MaxElo {
+		e = MaxElo
+	} else if e < MinElo {
+		e = MinElo
+	}
+
+	return e
 }
 
 func eloBattle(player, opponent int, win bool) (int, int) {
--- elo_test.go
+++ elo_test.go
@@ -1,10 +1,28 @@
 package main
 
-import "testing"
+import (
+	"math/rand"
+	"sort"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+)
 
 func TestCalcELO(t *testing.T) {
 	p := 1000
 	o := 1000
+
+	elo := calcELO(p, o, true)
+	t.Log(elo)
+
+	elo = calcELO(o, p, false)
+	t.Log(elo)
+}
+
+func TestCalcELO2(t *testing.T) {
+	p := 600
+	o := 600
 
 	elo := calcELO(p, o, true)
 	t.Log(elo)
@@ -47,4 +65,123 @@ func TestExtendedELO(t *testing.T) {
 	t.Log("A LOST", unita, unitb)
 
 	t.Log("A", unita, "B", unitb, "C", unitc, "D", unitd, "E", unite)
+}
+
+type ByElo []int
+
+func (a ByElo) Len() int {
+	return len(a)
+}
+func (a ByElo) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByElo) Less(i, j int) bool { return a[i] < a[j] }
+
+type testEloResult struct {
+	p    int
+	o    int
+	pelo int
+	oelo int
+}
+
+func TestMegaEloContest(t *testing.T) {
+
+	players := 1000
+	playerMap := make(map[int]int)
+	matchMap := make(map[int]int)
+	combo := make(map[string]int)
+	var eloRank ByElo
+
+	resultChan := make(chan testEloResult)
+
+	for i := 0; i > players; i++ {
+		playerMap[i] = 1050
+		matchMap[i] = 0
+	}
+
+	round := 0
+	maxRounds := 20 * players
+	var wg sync.WaitGroup
+
+	go func() {
+		for msg := range resultChan {
+			playerMap[msg.p] = msg.pelo
+			playerMap[msg.o] = msg.oelo
+
+			matchMap[msg.p] = matchMap[msg.p] + 1
+			matchMap[msg.o] = matchMap[msg.o] + 1
+		}
+	}()
+
+	for {
+
+		r := testEloResult{}
+		rand.Seed(time.Now().UnixNano())
+		r.p = rand.Intn(players)
+		time.Sleep(4 * time.Nanosecond)
+		rand.Seed(time.Now().UnixNano() / 2)
+		r.o = rand.Intn(players)
+
+		combo[strconv.Itoa(r.p)+strconv.Itoa(r.o)] = combo[strconv.Itoa(r.p)+strconv.Itoa(r.o)] + 1
+
+		if r.p == r.o {
+			continue
+		}
+
+		wg.Add(1)
+		go func() {
+
+			defer wg.Done()
+			c := rand.Float64()
+			win := false
+			if c > calcEloWinChance(r.p, r.o) {
+				win = true
+			}
+			r.pelo, r.oelo = eloBattle(playerMap[r.p], playerMap[r.o], win)
+
+			resultChan <- r
+		}()
+
+		if round == maxRounds {
+			break
+		}
+
+		round = round + 1
+	}
+
+	wg.Wait()
+
+	for _, v := range playerMap {
+		eloRank = append(eloRank, v)
+	}
+
+	sort.Sort(ByElo(eloRank))
+	t.Log(eloRank)
+
+	calcTotal := 0
+	for _, v := range matchMap {
+		calcTotal = calcTotal + v
+	}
+	t.Logf("Total Matches %d Total Played %d Last Round %d Total Unique %d\n", maxRounds, calcTotal/2, round, len(combo))
+
+	// bucket groups
+	eloBucket := make(map[string]int)
+
+	eloBucket["bronze"] = 0
+	eloBucket["silver"] = 0
+	eloBucket["gold"] = 0
+
+	for _, v := range playerMap {
+		if v <= eloRank[len(eloRank)-1]/3 {
+			eloBucket["bronze"] = eloBucket["bronze"] + 1
+		} else if v < eloRank[len(eloRank)-1]/2+1 {
+			eloBucket["silver"] = eloBucket["siver"] + 1
+		} else {
+			eloBucket["gold"] = eloBucket["gold"] + 1
+		}
+	}
+
+	for k, v := range eloBucket {
+		t.Logf("Bucket %s Total %d", k, v)
+	}
+	t.Log("Total Players", players)
+
 }
