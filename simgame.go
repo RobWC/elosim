6d94fd22f0157b9564e62863353479d1fb1392b7
Rob Cameron <rwcameron@gmail.com>
2016-06-26 01:39:27 -0700

    testing new random matchmaking

--- simgame.go
+++ simgame.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"sync"
 	"time"
@@ -15,6 +16,7 @@ const (
 type EloSim struct {
 	Players        map[uint64]*Player
 	MatchHistory   map[uint64]*Match
+	UniqueMatches  map[string]int
 	BaseElo        int
 	PendingMatches []*PendingMatch
 	matchMaker     func() *PendingMatch
@@ -24,8 +26,9 @@ type EloSim struct {
 // Create a new sim and set the base elo with be
 func NewEloSim(be int) *EloSim {
 	return &EloSim{Players: make(map[uint64]*Player),
-		MatchHistory: make(map[uint64]*Match),
-		BaseElo:      be}
+		MatchHistory:  make(map[uint64]*Match),
+		UniqueMatches: make(map[string]int),
+		BaseElo:       be}
 }
 
 // Start set elo sim background tasks
@@ -39,9 +42,7 @@ func (es *EloSim) Start() {
 
 // newPlayerID gernerate a new player ID
 func (es *EloSim) newPlayerID() uint64 {
-	dt := time.Now().UnixNano()
-
-	return uint64(dt)
+	return uint64(len(es.Players) + 1)
 }
 
 // newMatchID generate a new match ID
@@ -52,15 +53,16 @@ func (es *EloSim) newMatchID() uint64 {
 }
 
 // AddPlayer add an eligible player to the simulation
-func (es *EloSim) AddPlayer(p *Player) uint64 {
+func (es *EloSim) AddPlayer() uint64 {
 	newID := es.newPlayerID()
+	p := &Player{}
 	p.CreatedAt = time.Now()
 	p.Elo = es.BaseElo
 	p.ID = newID
 	if _, ok := es.Players[newID]; !ok {
 		es.Players[newID] = p
 	} else {
-		es.AddPlayer(p)
+		es.AddPlayer()
 	}
 	return newID
 }
@@ -77,32 +79,42 @@ func (es *EloSim) GenerateMatch() *PendingMatch {
 
 func (es *EloSim) RandomSelectPlayers() *PendingMatch {
 	pa, pb := uint64(0), uint64(0)
-	rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano() * rand.Int63() / 2)
 	pa = uint64(rand.Int63n(int64(len(es.Players))))
-	rand.Seed(time.Now().UnixNano() / rand.Int63())
+	rand.Seed(time.Now().UnixNano() * rand.Int63() * 2)
 	pb = uint64(rand.Int63n(int64(len(es.Players))))
 
 	return &PendingMatch{TeamA: []uint64{pa}, TeamB: []uint64{pb}}
 }
 
-func (es *EloSim) SimMatch(pa, pb *Player) {
-	match := &Match{ID: es.newMatchID()}
+func (es *EloSim) SimMatch(pm *PendingMatch) {
+	match := &Match{ID: es.newMatchID(), TeamA: pm.TeamA, TeamB: pm.TeamB}
 	match.Start()
 
-	win := false
-	if rand.Float64() > calcEloWinChance(pa.Elo, pb.Elo) {
-		win = true
-		match.TeamAWin()
-		es.Players[pa.ID].Wins = es.Players[pa.ID].Wins + 1
-		es.Players[pb.ID].Losses = es.Players[pb.ID].Losses + 1
-	} else {
-		match.TeamBWin()
-		es.Players[pb.ID].Wins = es.Players[pb.ID].Wins + 1
-		es.Players[pb.ID].Losses = es.Players[pa.ID].Losses + 1
+	if len(match.TeamA) == 1 && len(match.TeamB) == 1 {
+		//		win := false
+		pa := es.Players[match.TeamA[0]]
+		pb := es.Players[match.TeamB[0]]
+
+		// add unique match check
+		u := fmt.Sprintf("%X%X", match.TeamA[0], match.TeamB[0])
+		println(u)
+		es.UniqueMatches[u] = es.UniqueMatches[u] + 1
+
+		if rand.Float64() > calcEloWinChance(pa.Elo, pb.Elo) {
+			//			win = true
+			match.TeamAWin()
+			//			es.Players[pa.ID].Wins = es.Players[pa.ID].Wins + 1
+			//			es.Players[pb.ID].Losses = es.Players[pb.ID].Losses + 1
+		} else {
+			match.TeamBWin()
+			//			es.Players[pb.ID].Wins = es.Players[pb.ID].Wins + 1
+			//			es.Players[pb.ID].Losses = es.Players[pa.ID].Losses + 1
+		}
+		//		es.Players[pa.ID].Elo, es.Players[pb.ID].Elo = eloBattle(es.Players[pa.ID].Elo, es.Players[pb.ID].Elo, win)
 	}
-	es.Players[pa.ID].Elo, es.Players[pb.ID].Elo = eloBattle(es.Players[pa.ID].Elo, es.Players[pb.ID].Elo, win)
 
 	// add match history
 	match.Stop()
-	es.MatchHistory[match.ID] = match
+	//	es.MatchHistory[match.ID] = match
 }
--- simgame_test.go
+++ simgame_test.go
@@ -1,13 +1,18 @@
 package main
 
-import "testing"
+import (
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+)
 
 func TestBasicEloEloSim(t *testing.T) {
 	baseElo := 1200
 	es := NewEloSim(baseElo)
 	playerCount := 10000
 	for i := 0; i < playerCount; i++ {
-		es.AddPlayer(&Player{})
+		es.AddPlayer()
 	}
 	if len(es.Players) < playerCount {
 		t.Fail()
@@ -27,4 +32,34 @@ func TestBasicEloEloSim(t *testing.T) {
 		}
 		checkLimit = checkLimit + 1
 	}
+}
+
+func TestRandomMatchMakingEloSim(t *testing.T) {
+	baseElo := 1200
+	es := NewEloSim(baseElo)
+	playerCount := 10000
+	for i := 0; i < playerCount; i++ {
+		es.AddPlayer()
+	}
+	if len(es.Players) < playerCount {
+		t.Fail()
+	}
+
+	t.Logf("Found %d players", playerCount)
+
+	es.SetMatchMaking(es.RandomSelectPlayers)
+
+	var wg sync.WaitGroup
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			time.Sleep(time.Duration(rand.Intn(10)) * time.Nanosecond)
+			m := es.GenerateMatch()
+			t.Logf("%d", m)
+			es.SimMatch(m)
+		}()
+	}
+	wg.Wait()
+	t.Logf("Unique Matches %d Players %d", len(es.UniqueMatches), len(es.Players))
 }
